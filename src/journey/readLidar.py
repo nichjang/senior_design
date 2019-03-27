e40d59158259fc2fee3d691c2c85b99c680022f0
Ryan Mitchell <rmitchell97@utexas.edu>
2019-03-27 13:30:21 -0500

    small changes to help understand how to read the point cloud data type

--- src/journey/readLidar.py
+++ src/journey/readLidar.py
@@ -6,12 +6,24 @@ import rospy
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import PointCloud
 
+firstIteration = True
+
 #data.header.stamp.secs + data.header.stamp.nsecs for time
 #len(data.points) for the length of the data = number of points
 #data.points[i].(x y or z for the specific point)
 #callback = where it reads the data from the topic
 def callback(data):
-	print (data.points[0].x)
+	#print (data.points[0].x)
+	global firstIteration
+	if firstIteration:
+		firstIteration = False
+		for i in range(100):
+			print "data point: " + str(i)
+			print "x: " + str(data.points[i].x)
+			print "y: " + str(data.points[i].y)
+			print "z: " + str(data.points[i].z)
+			print		
+
 
 def listener():
 	#used to connect to the ROS Master node (replace 10.145.236.187:11311 with Master host IP)
@@ -22,7 +34,8 @@ def listener():
 
 	#keep reading ths subscriber continuously
 	rospy.spin()
-	
+
+		
 
 if __name__ == '__main__':
 	listener()
