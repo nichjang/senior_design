86468a2685c2eeb105bf869678e4e24b2e0a14f8
Nicholas <nich.jang@gmail.com>
2019-04-15 22:55:06 -0500

    update to moveit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nicholas/senior_design/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/nicholas/senior_design/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nicholas/senior_design/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/nicholas/senior_design/src:/home/nicholas/catkin_ws/src:/opt/ros/kinetic/share"
